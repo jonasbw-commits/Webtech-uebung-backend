b0de9007b353516c1703971bc66c0e2ebd64bff3
Paula Betz <paula.betz@gmx.de>
2026-05-05 19:40:21 +0200

    add CORS

--- src/main/java/com/example/demo/CardController.java
+++ src/main/java/com/example/demo/CardController.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cards")
 public class CardController {
